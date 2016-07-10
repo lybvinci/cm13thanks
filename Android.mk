14953737afa5757a798f5f85b0c714fc11f7aef1
lybvinci <1018466460@qq.com>
2016-07-10 21:48:44 +0800

    add Android.mk

--- Android.mk
+++ Android.mk
@@ -1,1 +1,11 @@
-LOCAL_PACKAGE_NAME := com.lybvinci.thanks
+LOCAL_PATH := $(call my-dir)
+  include $(CLEAR_VARS)
+   
+  # Build all java files in the java subdirectory-->直译（建立在java子目录中的所有Java文件）
+  LOCAL_SRC_FILES := $(call all-subdir-java-files)
+   
+  # Name of the APK to build-->直译（创建APK的名称）
+  LOCAL_PACKAGE_NAME := CM13thanks
+   
+  # Tell it to build an APK-->直译（告诉它来建立一个APK）
+  include $(BUILD_PACKAGE)
